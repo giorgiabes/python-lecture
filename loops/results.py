bb42793021e4f3cbdf6e5ebd0ebef4996608fd43
Giorgi Abesadze <abesadze.george@gmail.com>
2026-03-13 17:18:14 +0000

    add results.py

---
+++ loops/results.py
@@ -0,0 +1,20 @@
+results = ["Mario", "Luigi"]
+
+results.append("Princess")
+results.append("Yoshi")
+results.append("Koopa Troopa")
+results.append("Toad")
+
+
+results.append(["Bowser", "Donkey Kong Jr."])
+results.remove(["Bowser", "Donkey Kong Jr."])
+results.extend(["Bowser", "Donkey Kong Jr."])
+
+
+results.remove("Bowser")
+results.insert(0, "Bowser")
+print(results)
+
+
+results.reverse()
+print(results)
