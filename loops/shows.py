59ea51a0c013a0fc1bc4f2385af6f306c75c686a
Giorgi Abesadze <abesadze.george@gmail.com>
2026-03-13 15:57:50 +0000

    add shows.py

---
+++ loops/shows.py
@@ -0,0 +1,11 @@
+SHOWS = [
+    "Avatar: The last airbender",
+    "Ben 10",
+    "Arthur",
+    "Spongebob Squarepants",
+    "Phineas and ferb",
+    "Kim possible",
+    "Jimmy Neutron",
+    "the Proud family "
+]
+
