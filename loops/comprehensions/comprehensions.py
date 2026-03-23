0dc92e6501fe2d0e99b2e6e05f044afb407ded33
giorgiabes <abesadze.george@gmail.com>
2026-03-23 21:16:19 +0400

    add list and dictionary comprehensions

--- loops/comprehensions/comprehensions.py
+++ loops/comprehensions/comprehensions.py
@@ -2,8 +2,10 @@ from helpers import get_words, save_counts
 
 
 def main():
-    counts = {}
     words = get_words("address.txt")
+    words = [word.lower() for word in words if len(word) > 4]
+
+    counts = {word: words.count(word) for word in words}
 
     for word in words:
         if word in counts:
